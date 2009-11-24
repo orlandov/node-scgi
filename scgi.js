4035a59f46bd6309c84fd3b23ee2363c648606fe
Orlando Vazquez <ovazquez@gmail.com>
2009-11-24 11:08:06 -0800

    move receive callback into the conneciton listener callback

--- scgi.js
+++ scgi.js
@@ -2,7 +2,7 @@ process.mixin(GLOBAL, require('sys'));
 
 exports.createServer = function(requestListener) {
     var server = new process.tcp.Server();
-    
+   
     server.addListener('request', requestListener);
     server.addListener('connection', connectionListener);
 
@@ -12,24 +12,28 @@ exports.createServer = function(requestListener) {
 function connectionListener(connection) {
     puts('got new connection');
 
-    connection.addListener('receive', receiveListener);
+    connection.addListener('receive', function (packet) {
+        puts('got packet ' + packet);
+        var colon_idx = packet.indexOf(':');
+        var header_len = Number(packet.substr(0, colon_idx));
+
+        // TODO assert header_len =~ /\d+/
+        
+        var headers_chunk = packet.substr(colon_idx+1, colon_idx+header_len);
+        var headers_list = headers_chunk.split("\000");
+        var headers = {}
+
+        // TODO assert headers_list.length % 2 == 0
+
+        for (var i=0; i < headers_list.length; i += 2) {
+            headers[headers_list[i]] = headers_list[i+1];
+        }
+
+        connection.server.emit('request', headers);
+    });
 }
 
-function receiveListener(data) {
-    puts('got data ' + data);
-    var colon_idx = data.indexOf(':');
-    var header_len = Number(data.substr(0, colon_idx));
-    // TODO assert header_len =~ /\d+/
-    
-    var headers_chunk = data.substr(colon_idx+1, colon_idx+header_len);
-    var headers_list = headers_chunk.split("\000");
-    var headers = {}
-
-    // TODO assert headers_list.length % 2 == 0
-    for (var i=0; i < headers_list.length; i += 2) {
-        headers[headers_list[i]] = headers_list[i+1];
-    }
-    p(headers);
-}
-
-exports.createServer(function () {}).listen('8000');
+exports.createServer(function () {
+    puts("Haw, got here"+ JSON.stringify({nuts:arguments}));        
+    p(arguments);
+}).listen('8000');
